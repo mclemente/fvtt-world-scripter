c0905795626592c0b3278ccd73ad2ef1ec64df33
Matheus Clemente <matheus.xb@gmail.com>
2025-06-26 02:10:07 -0300

    Fix for weird FVTT bug

--- module/world-scripter.js
+++ module/world-scripter.js
@@ -13,3 +13,25 @@ Hooks.on("init", () => {
 		ui.notifications.error("WORLD_SCRIPTER.ERROR.Syntax", { localize: true });
 	}
 });
+
+Hooks.on("renderSettingsConfig", (_app, form) => {
+	const input = form.querySelector("code-mirror[name='world-scripter.scripts']");
+
+	const hint = `<p class="hint">${game.i18n.localize("WORLD_SCRIPTER.SETTINGS.scripts.hint")}</p>`;
+	const div = document.createElement('div');
+	div.innerHTML = hint.trim();
+	input.parentElement.nextElementSibling.replaceWith(div.firstElementChild)
+
+	// Weird workaround to fix the input's value getting extra indents
+	const value = game.settings.get("world-scripter", "scripts");
+	const { name: label, type: field } = game.settings.settings.get("world-scripter.scripts");
+	const scriptsInput = field.toInput({
+		hash: {
+			aria: { label },
+			elementType: "code-mirror",
+			language: "javascript",
+		},
+		value
+	})
+	input.replaceWith(scriptsInput);
+});
